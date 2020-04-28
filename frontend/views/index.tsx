9470836264d599fb8ee875f0ade12c7cd74ee108
JoshAshby <joshuaashby@joshashby.com>
2020-04-27 18:01:45 -0600

    learning how to manage state and such with crankjs

--- frontend/views/index.tsx
+++ frontend/views/index.tsx
@@ -1,5 +1,5 @@
 /** @jsx createElement */
-import { createElement, Context } from "@bikeshaving/crank"
+import { createElement, Context, Fragment } from "@bikeshaving/crank"
 import { renderer } from "@bikeshaving/crank/dom"
 
 async function wait(duration: number) {
@@ -11,25 +11,95 @@ async function* LoadingIndicator() {
   yield <div>Hang on, loading ...</div>
 }
 
-async function* ScrapeConfigs(this: Context) {
-  let data: any
+async function ScrapeConfigTable(this: Context) {
+  const res = await fetch("/api/v1/scrape_configs")
+  const data = await res.json()
 
-  const load = async () => {
-    const res = await fetch("/api/v1/scrape_configs")
-    data = await res.json()
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>URL</th>
+          <th># Extractions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.scrape_configs.map((scrape_config: any, idx: number) => (
+          <tr crank-key={idx}>
+            <td>{scrape_config.id}</td>
+            <td>{scrape_config.url}</td>
+            <td>{scrape_config.extraction_selectors.length}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
-    this.refresh()
-  }
+interface FormDataEvent extends Event {
+  readonly formData: FormData
+}
 
-  await load()
+interface FormDataEventInit extends EventInit {
+  formData: FormData
+}
 
-  for await ({} of this) {
+declare var FormDataEvent: {
+  prototype: FormDataEvent
+  new (type: string, eventInitDict?: FormDataEventInit): FormDataEvent
+}
+
+function* ScrapeConfigForm(this: Context) {
+  this.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const target = event.target as HTMLFormElement
+    new FormData(target)
+
+    target.reset()
+  })
+
+  this.addEventListener("formdata", (event) => {
+    const { formData } = event as FormDataEvent
+
+    const scrape_config: any = {
+      url: formData.get("url"),
+      extraction_selectors: formData
+        .get("extraction_selectors")
+        ?.toString()
+        .split(",")
+        .map((s) => s.trim()),
+    }
+
+    fetch("/api/v1/scrape_configs", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(scrape_config),
+    })
+  })
+
+  while (true) {
     yield (
-      <p onclick={load}>
-        There are {data.scrape_configs.length} scrape configs.
-      </p>
+      <form>
+        <input required type="url" name="url" />
+        <input type="text" name="extraction_selectors" />
+
+        <input type="submit" />
+      </form>
     )
   }
+}
+
+function ScrapeConfigs() {
+  return (
+    <Fragment>
+      <ScrapeConfigForm />
+      <ScrapeConfigTable />
+    </Fragment>
+  )
 }
 
 async function* App(this: Context) {
@@ -39,4 +109,5 @@ async function* App(this: Context) {
   }
 }
 
-renderer.render(<App />, document.body)
+const root = document.getElementById("app-root") as Element
+renderer.render(<App />, root)
