168c470914009ef569e10a70876d2a45bf251980
JoshAshby <joshuaashby@joshashby.com>
2020-04-30 22:39:44 -0600

    fill out API for what the ext needs

--- routes/api.rb
+++ routes/api.rb
@@ -13,18 +13,27 @@ class Api < Roda
 
     r.on "v1" do
       r.on "site" do
-        r.is do
-          r.get do
-            {
-              scrape_config: ScrapeConfig.find_or_create(url: r.params["url"]).values
-            }
-          end
+        r.get do
+          {
+            scrape_config: ScrapeConfig.find_or_create(url: r.params["url"]).values
+          }
         end
       end
 
-      r.on "scrape_configs" do
+      r.on "scrape_config" do
         r.on String do |id|
           @scrape_config = ScrapeConfig[id]
+
+          r.on "extraction" do
+            r.on Integer do |idx|
+              r.delete do
+                @scrape_config.extraction_selectors.delete_at(idx)
+                @scrape_config.save
+
+                { scrape_config: @scrape_config.values }
+              end
+            end
+          end
 
           r.get do
             { scrape_config: @scrape_config.values }
@@ -32,29 +41,30 @@ class Api < Roda
 
           r.patch do
             @scrape_config.update r.params
+            @scrape_config.save
+
             { scrape_config: @scrape_config.values }
           end
 
           r.delete do
             @scrape_config.delete
+
             { scrape_config: @scrape_config.values, deleted: true }
           end
         end
 
-        r.is do
-          r.get do
-            {
-              scrape_configs: ScrapeConfig.all.map(&:values)
-            }
-          end
+        r.get do
+          {
+            scrape_configs: ScrapeConfig.all.map(&:values)
+          }
+        end
 
-          r.post do
-            scrape_config = ScrapeConfig.create r.params
+        r.post do
+          scrape_config = ScrapeConfig.create r.params
 
-            {
-              scrape_config: scrape_config.values
-            }
-          end
+          {
+            scrape_config: scrape_config.values
+          }
         end
       end
     end
